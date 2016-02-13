8e9409998417c1b33f0d3a1306b09c481bc2eab1
alextobias48 <alextobias48@gmail.com>
2016-02-13 12:54:17 -0600

    Reads data into table

--- src/apriori.java
+++ src/apriori.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class apriori {
 
@@ -12,21 +13,39 @@ public class apriori {
 		double min_support = Double.parseDouble(args[1]);
 		double min_confidence = Double.parseDouble(args[2]);
 		
+		int numRows = 0;
+		int numAttributes = 0;
+		
+		Object [][] data = new Object[500][50];
+		String[] columnNames = new String[50];
+		
 		while(reader.ready()){
 			String line = reader.readLine();
 			if(line.startsWith("@relation")){
-				
+				continue;
 			}else if(line.startsWith("@attribute")){
+				columnNames[numAttributes] = line.split(" ")[1];
+				numAttributes++;
 				
 			}else if(line.startsWith("@data")){
-				
+				continue;
 			}else if(line.isEmpty()){
-				
+				continue;
 			}else{
-				
+				String[] rowValues = line.split(",");
+				for(int i = 0; i < rowValues.length; i++){
+					data[numRows][i] = rowValues[i];
+					
+				}
+				numRows++;			
 			}
-			
 		}
+		
+		System.out.println("Number of Rows: " + numRows);
+		System.out.println("Number of Attributes: " + numAttributes);
+		System.out.print(data[400][16]);
+		
+
 	}
 
 }
