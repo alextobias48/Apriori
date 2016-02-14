1fa9c351622102b417933d7f981f3e51866587ca
alextobias48 <alextobias48@gmail.com>
2016-02-14 16:21:02 -0600

    Builds L2

--- src/apriori.java
+++ src/apriori.java
@@ -18,7 +18,7 @@ public class apriori {
 
 		int numRows = 0;
 		int numAttributes = 0;
-		int count = 0;
+		Double count = 0.0;
 
 		Object[][] data = new Object[500][50];
 		Attribute[] columnNames = new Attribute[50];
@@ -53,44 +53,102 @@ public class apriori {
 			}
 		}
 
-		// Construct C1
 
-		// Loop for all atributes
-		// Loop for all potential values
-		// Loop through column to count instances
-
-		// Add attribute-potential value and count
-
-
-		Map<String, Integer> C1 = new HashMap<String, Integer>();
+		Map<String, Double> C1 = new HashMap<String, Double>();
 
 		for (int a = 0; a < numAttributes; a++) {
-			for (int b = 0; b < numRows; b++) {
+			for (int j = 0; j < columnNames[a].potentialValues.size(); j++) {
+				
+				for (int b = 0; b < numRows; b++) {
 
-
-				for (int j = 0; j < columnNames[a].potentialValues.size(); j++) {
+				
 					if ((data[b][a]).toString().equalsIgnoreCase(columnNames[a].potentialValues.get(j))) {
 						count++;
 
-						Integer x = C1.get(columnNames[a].potentialValues.get(j));
-						if (x != null) {
-							C1.put(columnNames[a].potentialValues.get(j), x+count);
-						} else 
-						{
-							C1.put(columnNames[a].potentialValues.get(j), count);
-						}
+						//Integer x = C1.get(columnNames[a].potentialValues.get(j));
+						//if (x != null) {
+							//C1.put(columnNames[a].potentialValues.get(j), x+count);
+						//} else 
+						//{
+							//C1.put(columnNames[a].potentialValues.get(j), count);
+						//}
 
 					}
+					
 				}
-				count = 0;
+				C1.put(columnNames[a].attributeName + ":" + columnNames[a].potentialValues.get(j), count);
+					count = 0.0;
+
 
 			}
-			for (Map.Entry<String, Integer> entry : C1.entrySet()) {
-				System.out.printf("Key : %s and Value: %d %n", entry.getKey(), entry.getValue());
-			}
-			C1.clear();
+
 		}
-		System.out.println("Number of Rows: " + numRows);
+		
+		Map<String, Double> L1 = new HashMap<String, Double>();
+
+		
+		for (Map.Entry<String, Double> entry : C1.entrySet()) {
+			if(entry.getValue() / numRows > min_support){
+				L1.put(entry.getKey(), entry.getValue());
+			}
+		}
+		
+		Map<String, Double> C2 = new HashMap<String, Double>();
+		
+		for(Map.Entry<String, Double> entry : L1.entrySet()){
+			for(Map.Entry<String, Double> entry2 : L1.entrySet()){
+				count = 0.0;
+				if(entry != entry2){
+					String colName = entry.getKey().split(":")[0];
+					String value = entry.getKey().split(":")[1];
+										
+					String col2Name = entry2.getKey().split(":")[0];
+					String value2 = entry2.getKey().split(":")[1];
+					
+					int col1 = -1;
+					int col2 = -1;
+									
+					for(int k = 0; k < numAttributes; k++){
+						if(columnNames[k].attributeName.equalsIgnoreCase(colName)){
+							col1 = k;
+						}else if(columnNames[k].attributeName.equalsIgnoreCase(col2Name)){
+							col2 = k;
+						}
+					}
+					
+					for (int b = 0; b < numRows; b++) {
+						
+						
+						if(String.valueOf(data[b][col1]).equalsIgnoreCase(value)){
+							if(String.valueOf(data[b][col2]).equalsIgnoreCase(value2)){
+								count++;
+							}
+						}
+						
+					}
+					
+					C2.put(entry.getKey() + ">" + entry2.getKey(), count);
+				}
+			}
+		}
+		
+		Map<String, Double> L2 = new HashMap<String, Double>();
+		
+		for (Map.Entry<String, Double> entry : C2.entrySet()) {
+			
+			if(entry.getValue() / numRows > min_support){
+				if((entry.getValue() / numRows) / (L1.get(entry.getKey().split(">")[0]) / numRows) > min_confidence){
+					L2.put(entry.getKey(), entry.getValue());
+				}
+			}
+		}
+
+		for(Map.Entry<String, Double> entry : L2.entrySet()){
+			System.out.printf("Key : %s and Value: %f %n", entry.getKey(), entry.getValue());
+
+		}
+		
+		System.out.println("Number of Rows L2: " + L2.size());
 		System.out.println("Number of Attributes: " + numAttributes);
 
 
